a9242cdca8b9d4aec643b0f02d387538ebe7f860
Michiel <Michiel.De.Cuyper@codacio.us>
2019-11-22 16:36:03 +0100

    Rename namespaces

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace lunchbox_webapi
+namespace Lunchbox
 {
     public class Program
     {
